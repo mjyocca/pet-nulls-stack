d32de8e3f83f869e55cb8ac49feb2cc408e29580
Michael Yocca <19916665+mjyocca@users.noreply.github.com>
2024-05-20 13:21:13 -0700

    Update main.tf

--- pet/main.tf
+++ pet/main.tf
@@ -8,20 +8,20 @@ terraform {
       version = "3.3.2"
     }
 
-    derp = {
-      source = "tfcdev-86ee0655.ngrok.io/hashicorp/derp"
-      version = "1.0.3"
-    }
+#    derp = {
+#      source = "tfcdev-86ee0655.ngrok.io/hashicorp/derp"
+#      version = "1.0.3"
+#    }
   }
 }
 
-data "derp_example" "example" {
-  configurable_attribute = "some-value"
-}
+#data "derp_example" "example" {
+#  configurable_attribute = "some-value"
+#}
 
-data "derp_example" "example_2" {
-  configurable_attribute = "some-value"
-}
+#data "derp_example" "example_2" {
+#  configurable_attribute = "some-value"
+#}
 
 module "echo" {
   source  = "tfcdev-86ee0655.ngrok.io/hashicorp/echo/null"
